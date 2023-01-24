5f3314aedab9f6aef4c8fdd865f53540ea541fe0
xmasnoaa <snowdevnoa@gmail.com>
2023-01-23 23:15:17 -0600

    foundational components

---
+++ src/Nav.jsx
@@ -0,0 +1,12 @@
+function Navbar() {
+  return (
+    <nav className="flex center se">
+      <a>HOME</a>
+      <a>ABOUT</a>
+      <a>WORKS</a>
+      <a>CONTACT</a>
+    </nav>
+  );
+}
+
+export default Navbar;
