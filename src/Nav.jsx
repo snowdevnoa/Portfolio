be27d22b1818827cccd728dc1d1f5cda29c5f1b5
xmasnoaa <snowdevnoa@gmail.com>
2023-01-28 12:32:12 -0600

    utilizing tailwind css

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,6 +1,6 @@
 function Navbar() {
   return (
-    <nav className="flex center se">
+    <nav className="w-full py-5 flex justify-evenly">
       <a>HOME</a>
       <a>ABOUT</a>
       <a>WORKS</a>
