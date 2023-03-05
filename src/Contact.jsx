5afcb2e2acd3d7ec9d928f75c3188ebe5cc2e1f8
xmasnoaa <snowdevnoa@gmail.com>
2023-03-05 13:03:04 -0600

    formspree integration and contact form functionality

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -17,9 +17,18 @@ export default function Contact() {
       [name]: value,
     }));
   }
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
-    setFormData((prevState) => ({ ...prevState, completed: true }));
+    const response = await fetch('https://formspree.io/f/xgebydyj', {
+      method: 'POST',
+      body: JSON.stringify(formData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    if (response.ok) {
+      setFormData((prevState) => ({ ...prevState, completed: true }));
+    }
   }
 
   return (
@@ -29,39 +38,47 @@ export default function Contact() {
     >
       <div className="flex flex-col items-center justify-center md:w-1/2">
         <h1 className="text-[3rem]">LET'S CONNECT!</h1>
-        <p className="mt-[2rem]">I'm a people person, not a predator. :)</p>
+        <p className="mt-[2rem]">I'm a people person, not a predator :)</p>
       </div>
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-col w-[500px] p-[2.5rem] mt-[-11rem] md:mt-0 md:w-1/2"
-      >
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          onChange={handleChange}
-          name="name"
-          value={formData.name}
-        />
-        <label htmlFor="email" className="mt-3">
-          Email:
-        </label>
-        <input
-          type="email"
-          onChange={handleChange}
-          name="email"
-          value={formData.email}
-        />
-        <label htmlFor="message" className="mt-3">
-          Message:
-        </label>
-        <textarea
-          onChange={handleChange}
-          name="message"
-          value={formData.message}
-          placeholder="How may I be of assistance?"
-        />
-        <button>Send Message</button>
-      </form>
+      {formData.completed ? (
+        <div className="md:w-1/2 text-center w-[500px] p-[2.5rem] mt-[-11rem] md:mt-0 md:w-1/2">
+          <h1 className="text-[2rem] leading-[1]">
+            Thank you for connecting with me, talk to you soon!
+          </h1>
+        </div>
+      ) : (
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col w-[500px] p-[2.5rem] mt-[-11rem] md:mt-0 md:w-1/2"
+        >
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            onChange={handleChange}
+            name="name"
+            value={formData.name}
+          />
+          <label htmlFor="email" className="mt-3">
+            Email:
+          </label>
+          <input
+            type="email"
+            onChange={handleChange}
+            name="email"
+            value={formData.email}
+          />
+          <label htmlFor="message" className="mt-3">
+            Message:
+          </label>
+          <textarea
+            onChange={handleChange}
+            name="message"
+            value={formData.message}
+            placeholder="How may I be of assistance?"
+          />
+          <button type="submit">Send Message</button>
+        </form>
+      )}
     </div>
   );
 }
