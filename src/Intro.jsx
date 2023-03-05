38a11897b5ab9506e166a67ef32d149288a340d9
xmasnoaa <snowdevnoa@gmail.com>
2023-03-05 16:15:30 -0600

    Update Intro.jsx

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -2,12 +2,7 @@ function Intro() {
   return (
     <div className="min-h-screen flex flex-wrap md:flex-no-wrap intro">
       <div className="w-full md:w-3/5 flex justify-center items-center animate-hide">
-        <iframe
-          src="https://app.vectary.com/p/4srKhyhUBURMcYPK9PNZ00"
-          frameborder="0"
-          width="100%"
-          height="480"
-        ></iframe>
+        NEW PORTFOLIO PLACEHOLDER
       </div>
       <div className="w-full md:w-2/5 flex flex-col justify-center items-center animate-hide">
         <h1 className="text-9xl mb-12">NOA</h1>
