257d9b93eaf7c7dd202684a1e7de35f1b58e6811
xmasnoaa <snowdevnoa@gmail.com>
2023-04-03 17:56:00 -0500

    color style adjustments

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -7,7 +7,7 @@ function Intro() {
         3D OBJECT IN PROGRESS
       </div>
       <div className="w-full md:w-2/5 flex flex-col justify-center items-center animate-hide">
-        <h1 className="text-9xl mb-12">NOA</h1>
+        <h1 className="text-9xl mb-12 text-gradient">NOA</h1>
         <h3 className="px-10 text-center">
           Developer, Designer, Jazz Enthusiast
         </h3>
